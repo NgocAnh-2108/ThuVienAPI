203ca5eb6f724cc7550a30fa7b49a4005918cf02
Anh <hoangngocanh20005@gmail.com>
2026-09-23 15:41:31 +0700

    Phan2

---
+++ Phan1/Models/Domain/Publisher.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Phan1.Models.Domain
+{
+    public class Publisher
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Book> Books { get; set; }
+    }
+}
